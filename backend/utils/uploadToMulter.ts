3367cab6011f6c8f990555d75b6e85d14080fc00
Faraz <faraz9901>
2025-03-11 10:30:17 +0530

    the upload error issue - testing

--- backend/utils/uploadToMulter.ts
+++ backend/utils/uploadToMulter.ts
@@ -1,10 +1,16 @@
 import multer from 'multer';
 import path from 'path';
+import fs from 'fs';
 
 const upload = multer({
     storage: multer.diskStorage({
         destination: function (req, file, cb) {
             const filePath = path.join(__dirname, '..', 'uploads')
+
+            if (!fs.existsSync(filePath)) {
+                fs.mkdirSync(filePath, { recursive: true });
+            }
+
             cb(null, filePath)
         },
         filename: function (req, file, cb) {
